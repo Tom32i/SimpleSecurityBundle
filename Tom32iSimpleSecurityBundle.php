4ff6f795eaff10802c4fc6064d65afb0109ac732
tom32i <thomas.jarrand@gmail.com>
2014-09-25 20:33:49 +0200

    Fixed typo

--- Tom32iSimpleSecurityBundle.php
+++ Tom32iSimpleSecurityBundle.php
@@ -2,7 +2,7 @@
 
 namespace Tom32i\Bundle\SimpleSecurityBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle;
 
 class Tom32iSimpleSecurityBundle extends Bundle
 {
