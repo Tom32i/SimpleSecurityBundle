9d15b3296f4abcc2d7e2efd6fe1fdd9426a03a2d
Thomas Jarrand <thomas.jarrand@gmail.com>
2014-12-02 11:02:32 +0100

    Fixed login controller

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -24,23 +24,16 @@ class LoginController extends BaseController
         $session = $request->getSession();
 
         $user = [
-            '_username' => $session->get(SecurityContext::LAST_USERNAME),
-            '_password' => null,
-            '_remeber_me' => true
+            '_username'    => $session->get(SecurityContext::LAST_USERNAME),
+            '_password'    => null,
+            '_remember_me' => true
         ];
 
         $form = $this->createForm('login', $user, [
             'action' => $this->generateUrl('login_check')
         ]);
 
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-        } else {
-            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
-        }
-
-        if ($error) {
+        if ($error = $this->getSecurityError($request, SecurityContext::AUTHENTICATION_ERROR)) {
             if ($error instanceof LockedException) {
                 $code = 'login.error.locked';
             } elseif ($error instanceof DisabledException) {
@@ -55,5 +48,30 @@ class LoginController extends BaseController
         }
 
         return ['form' => $form->createView()];
+    }
+
+    /**
+     * Get security Error from request or session
+     *
+     * @param Request $request
+     * @param string $errorName
+     *
+     * @return Symfony\Component\Security\Core\Exception\AuthenticationException
+     */
+    private function getSecurityError(Request $request, $errorName)
+    {
+        if ($request->attributes->has($errorName)) {
+            return $request->attributes->get($errorName);
+        }
+
+        $session = $request->getSession();
+
+        if ($session->has($errorName)) {
+            $error = $session->get($errorName);
+
+            $session->remove($errorName);
+
+            return $error;
+        }
     }
 }
